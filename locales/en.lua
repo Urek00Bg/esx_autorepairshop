06afdd3d82a482ac26adbf47524e1ac92d895d16
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-05-18 20:06:56 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+Locales['en'] = {
+    ['start_message'] = 'I\'ll get right onto that',
+    ['success_message'] = 'Work done. Hope you enjoy!',
+
+    ['enoughlsc_message'] = 'Too many other mechanics on service. Sorry, I can\'t help',
+    ['nocar_message'] = 'You want me to repair your brain?'
+}
---
+++ locales/pl.lua
@@ -0,0 +1,7 @@
+Locales['pl'] = {
+    ['start_message'] = 'Już się zabieram za to autko',
+    ['success_message'] = 'Zrobione. Mi się podoba. A tobie?',
+
+    ['enoughlsc_message'] = 'Zbyt dużo innych mechaników na służbie. Przepraszam, nie mogę pomóc',
+    ['nocar_message'] = 'Nie masz auta, co mam ci naprawić? Mózg?'
+}
